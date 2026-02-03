60f7065e231a6e5a285d4551dae9661009d90154
kondabhaskar2509 <kondabhaskarreddy143@gmail.com>
2026-02-03 22:51:02 +0530

    commit

---
+++ client/src/config/env.js
@@ -0,0 +1,2 @@
+export const FRONTEND = import.meta.env.VITE_FRONTEND
+export const BACKEND = import.meta.env.VITE_BACKEND
--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ import cors from "cors";
 import mongodb from "mongodb";
 import setupOAuth from "./oauth.js";
 import setupAuth from "./auth.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const MongoClient = mongodb.MongoClient;
-const url ="mongodb://localhost:27017/";
+const url = process.env.MONGODB_URI;
 const port = 3000;
 
 app.use(cors());
@@ -24,12 +27,38 @@ MongoClient.connect(url, {
   .then(async (client) => {
     const clientcollection = client.db("booking").collection("clientdata");
     const myauthcollection = client.db("booking").collection("myauthusers");
-   
 
-    setupOAuth(app, clientcollection ,myauthcollection);
+    setupOAuth(app, clientcollection, myauthcollection);
     setupAuth(app, myauthcollection);
 
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
   });
+
+
+
+// (async () => {
+//   try {
+//     const client = await MongoClient.connect(url, {
+//       maxPoolSize: 50,
+//       wtimeoutMS: 2500,
+//     });
+
+//     console.log("Connected to MongoDB");
+
+//     const db = client.db("booking");
+//     const clientcollection = db.collection("clientdata");
+//     const myauthcollection = db.collection("myauthusers");
+
+//     setupOAuth(app, clientcollection, myauthcollection);
+//     setupAuth(app, myauthcollection);
+
+//     app.listen(port, () => {
+//       console.log(`Server listening on port ${port}`);
+//     });
+//   } catch (err) {
+//     console.error("Mongo connection error:", err);
+//     process.exit(1);
+//   }
+// })();
